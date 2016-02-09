6ab61571bfe61866d537a5aad835ae55376c5183
Nachtfalter <f.graier@gmx.de>
2016-02-09 01:46:25 +0100

    Imported Landscape

--- Infinity.ocf/Pilottraining.ocs/Script.c
+++ Infinity.ocf/Pilottraining.ocs/Script.c
@@ -1,20 +1,24 @@
 /*-- Pilotrace --*/
 
+protected func InitializePlayer() {
+    //SetFoW (false);
+ }
+ 
 protected func Initialize()
 {
-	// Create parkour goal & checkpoints.
+	/*// Create parkour goal & checkpoints.
 	var goal = CreateObject(Goal_Parkour, 0, 0, NO_OWNER);
 	var mode = PARKOUR_CP_Respawn | PARKOUR_CP_Check | PARKOUR_CP_Ordered;
 	goal->SetStartpoint(90, 345);
 	goal->AddCheckpoint(630, 215, mode);
 	goal->AddCheckpoint(211, 105, mode);
 	goal->SetFinishpoint(80, 345);
-	
-	// weil Baum
-	for(var i = 1; i <= 3; ++i)
-		PlaceVegetation(Tree_Coniferous3, 0, 0, 1000, 1000, -1);
+	*/
+
+	PlaceGrass(1000);
 }
 
+/*
 // Gamecall from parkour goal, on respawning.
 protected func OnPlayerRespawn(int plr, object cp)
 {
@@ -26,4 +30,4 @@ global func FxRespawnBoomTimer(object target, effect, int time)
 {
 	target->CreateContents(Boompack);
 	return -1;
-}
+}*/
