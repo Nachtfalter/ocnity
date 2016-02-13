35c8a24c292cf38ba94c7e15ca5710f8e8783515
Lukas Werling <lukas.werling@gmail.com>
2016-02-13 12:55:29 +0100

    Add three check points

--- Infinity.ocf/Pilottraining.ocs/Script.c
+++ Infinity.ocf/Pilottraining.ocs/Script.c
@@ -1,20 +1,28 @@
 /*-- Pilotrace --*/
 
 protected func InitializePlayer() {
-    //SetFoW (false);
+	//SetFoW (false);
 }
 	
-protected func Initialize()
-{
+protected func Initialize() {
 	// Create parkour goal & checkpoints.
 	var goal = CreateObject(Goal_Parkour, 0, 0, NO_OWNER);
 	var mode = PARKOUR_CP_Respawn | PARKOUR_CP_Check | PARKOUR_CP_Ordered;
-	goal->SetStartpoint(50, 140);
-	goal->SetFinishpoint(3145, 145);
+	var checkpoint_padding = 50;
+	goal->SetStartpoint(checkpoint_padding, 140);
+	goal->SetFinishpoint(LandscapeWidth() - checkpoint_padding, 145);
 	
+	var checkpoint_count = 3;
+	var checkpoint_distance = (LandscapeWidth() - 2 * checkpoint_padding) / (checkpoint_count + 1);
+	for (var i = 1; i <= checkpoint_count; i++) {
+		goal->AddCheckpoint(
+			checkpoint_padding + i * checkpoint_distance,
+			RandomX(checkpoint_padding, LandscapeHeight() - checkpoint_padding),
+			mode);
+	}
 
-	for (var count = 0; count < 20; count++)
-	{
+	//  Place Drafts
+	for (var count = 0; count < 20; count++) {
 		var draft = CreateObject(Draft);
 		draft->SetR(Random(360));
 	}
@@ -23,10 +31,8 @@ protected func Initialize()
 	PlaceGrass(1000);
 }
 
-
 // Gamecall from parkour goal, on respawning.
-protected func OnPlayerRespawn(int plr, object cp)
-{
+protected func OnPlayerRespawn(int plr, object cp) {
 	var clonk = GetCrew(plr);
 	clonk->CreateContents(Shovel);
 }
